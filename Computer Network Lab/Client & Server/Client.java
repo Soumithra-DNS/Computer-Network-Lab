3123ae4afb7c7ba141009d6e8dcd98d3442d0fea
Biologist <soumithradebnathshanto@gmail.com>
2026-04-28 20:13:12 +0600

    first commit

--- Computer Network Lab/Client & Server/Client.java
+++ Computer Network Lab/Client & Server/Client.java
@@ -1,13 +1,21 @@
-import java.io.DataOutputStream;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class Client {
     public static void main(String[] args) throws Exception {
 
-        Socket s2 = new Socket("localhost", 7777);
-        DataOutputStream ds2 = new DataOutputStream(s2.getOutputStream());
-        ds2.writeUTF("How Are You??");
+        Socket s = new Socket("localhost", 7777);
 
-        s2.close();
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+        // Send message to server
+        dos.writeUTF("How are you Server?");
+
+        // Receive reply from server
+        String serverMsg = dis.readUTF();
+        System.out.println("Server says: " + serverMsg);
+
+        s.close();
     }
 }
